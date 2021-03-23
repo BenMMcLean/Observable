006403b0d043ec2fc9c869f471643dc53945b0a9
Ben McLean <ben.mclean@stripysock.com.au>
2021-03-24 02:18:49 +1100

    Added AsyncTransformationTest

---
+++ base/src/test/java/cl/benm/observable/AsyncTransformTest.kt
@@ -0,0 +1,57 @@
+package cl.benm.observable
+
+import cl.benm.observable.concrete.SingleValueObservable
+import cl.benm.observable.unwrap.SimpleAsyncTransformation
+import cl.benm.observable.unwrap.SimpleObserver
+import cl.benm.observable.unwrap.SimpleTransformation
+import org.junit.Assert
+import org.junit.Test
+
+class AsyncTransformTest {
+
+    @Test(timeout = 500)
+    fun singleTransform() {
+        val emitter = SingleValueObservable<Int>(ExceptionOrValue.Value(0))
+            .transformAsync(object: SimpleAsyncTransformation<Int, Int>() {
+                override fun transformSuccess(`in`: Int?): Observable<Int> {
+                    return SingleValueObservable(ExceptionOrValue.Value(`in`!!+1))
+                }
+            })
+        emitter.observe(object: SimpleObserver<Int>() {
+            override fun onSuccess(value: Int?) {
+                Assert.assertEquals(value, 1)
+            }
+
+            override fun onFailure(throwable: Throwable?) {
+                throw throwable!!
+            }
+        })
+    }
+
+    @Test(timeout = 500)
+    fun multipleTransform() {
+        val emissions = listOf(
+            0,
+            1,
+            5
+        )
+        var emissionIndex = 0
+
+        val emitter = EmissionOverTimeObservable<Int>(emissions.map { ExceptionOrValue.Value(it) }, 10)
+            .transformAsync(object: SimpleAsyncTransformation<Int, Int>() {
+                override fun transformSuccess(`in`: Int?): Observable<Int> {
+                    return SingleValueObservable(ExceptionOrValue.Value(`in`!!+1))
+                }
+            })
+        emitter.observe(object: SimpleObserver<Int>() {
+            override fun onSuccess(value: Int?) {
+                Assert.assertEquals(value, emissions[emissionIndex++]+1)
+            }
+
+            override fun onFailure(throwable: Throwable?) {
+                throw throwable!!
+            }
+        })
+    }
+
+}
